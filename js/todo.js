54ea4d51922bb8d3cdc8698a122cd5663dd20a93
Rex Lo <LORE2@OOCL.COM>
2018-11-14 16:37:51 +0800

    add item function & update list function

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,10 @@
 $(document)
     .ready(function () {
+	var list = [];
+	var filtering = "all";
+	const itemConstructor = item => `<li id=${item.id} class="${item.isComplete ? "checked" : ""}">
+									<input name="done-todo" ${item.isComplete ? 'checked' : ""} type="checkbox" class="done-todo" />
+									<span> ${item.name}</span> </li>`
 
         function generateUUID() {
             /*jshint bitwise:false */
@@ -22,4 +27,27 @@ $(document)
         }
 
         // code to be implemented
+		
+		//clear input field
+		function clearInput(){
+			$('input[name=ListItem]').val('');
+		}
+		
+		//add item
+        $('#button').click(add);
+		
+		function add(){
+			list.push({id: generateUUID(), name: $('input[name=ListItem]').val(), isComplete: false});
+			clearInput();
+			refreshList();
+		}	
+		
+		//update list
+		function refreshList(){
+			var filterList = item => [{filterKey: "all", return: true}, {filterKey: "complete", return: item.isComplete}, {filterKey: "active", return: !item.isComplete}]
+										.find(item => item.filterKey == filtering).return;
+			var orderlist = list.filter(filterList).map(item => itemConstructor(item));
+			$('ol').html(orderlist);
+		}
+		
     });
