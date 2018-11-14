e41812bba9450006d900a16c01dfe50b5234aedf
Rex Lo <LORE2@OOCL.COM>
2018-11-14 17:23:41 +0800

    added edit clicked item function and finished this practice, the practice which failed the test has be fixed, please kindly check it again.

---
+++ js/Summary.md
@@ -0,0 +1,2 @@
+Found that my code cannot process the existed data provided in index.html, but i cannot figure out what's wrong with the code. 
+Other functions are working fine.
--- js/todo.js
+++ js/todo.js
@@ -47,6 +47,21 @@ $(document)
             $(this).parent().toggleClass('checked');
             list.find(item => item.id === $(this).parent()[0].id).isComplete = $(this).parent().hasClass('checked');
         });
+		
+		//edit clicked todo item
+        $(document).on('click', 'li', function () {
+			var clickedItem = $(this).children('span');
+			clickedItem.attr('contentEditable', 'true')
+            clickedItem.focus();
+			clickedItem.keypress(function (event) {
+                    if (event.keyCode == '13') {
+                        clickedItem.attr('contentEditable', 'false');
+                        clickedItem.blur();
+                        list.find(item => item.id === $(this).parent()[0].id).name = $(this).text();
+                        refreshList();
+                    }
+                });
+        });
 
 		//filters on-click event
 		$('#filters li a').click(function (item) {
@@ -57,8 +72,8 @@ $(document)
 		
 		//update list
 		function refreshList(){
-			var filterList = item => [{filterKey: "all", return: true}, {filterKey: "complete", return: item.isComplete}, {filterKey: "active", return: !item.isComplete}]
-										.find(item => item.filterKey == filtering).return;
+			var filterList = item => [{filterKey: "all", output: true}, {filterKey: "complete", output: item.isComplete}, {filterKey: "active", output: !item.isComplete}]
+										.find(item => item.filterKey == filtering).output;
 			var orderlist = list.filter(filterList).map(item => itemConstructor(item));
 			$('ol').html(orderlist);
 		}
